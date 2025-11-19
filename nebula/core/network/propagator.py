839ce98a358805d249bcebd7b7d0001bc9332c24
Felix <fstoeger@ethz.ch>
2025-11-19 21:52:22 +0100

    Changed position of DSCP argument in the call to send_message, in accordance with previous commit.

--- nebula/core/network/propagator.py
+++ nebula/core/network/propagator.py
@@ -382,7 +382,7 @@ class Propagator:
             for message in messages:
                 # CHANGE: Message type changed from "model" to "modellayer".
                 # TODO: Change hardcoded DSCP value to be dynamically computed.
-                asyncio.create_task(self.cm.send_message(neighbor_addr, 0b11101000 ,message, "modellayer"))
+                asyncio.create_task(self.cm.send_message(neighbor_addr ,message, 0b11101000, "modellayer"))
             
             # OLD CODE, has been replaced with per-layer sending
             # logging.info(
